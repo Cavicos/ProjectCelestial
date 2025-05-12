e622b68c26f88c3b8d7707b6cc7d49eb1df17ea2
Cavicos <nimbignoob@gmail.com>
2025-05-12 21:20:59 +0200

    Create Celestial.lua

---
+++ Celestial.lua
@@ -0,0 +1,4 @@
+if getgenv().MainKey == "nil" then
+getgenv().MainLoad = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/petscripters/ProjectCelestial/refs/heads/main/CelestialHub"))() 
+end
